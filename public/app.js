f319f412a17bd9f362f637e30962cc65e4931c2e
Zane Shaw <admin@triangle100.com>
2022-09-18 13:32:13 +0000

    Add debug section

--- public/app.js
+++ public/app.js
@@ -1,5 +1,13 @@
-fetch(`${window.location.href}stats`)
-	.then((res) => res.json())
-	.then((data) => {
-		console.log(data);
-	});
+const debugOutput = document.getElementById("debug");
+
+function init() {
+	fetch(window.location.href + "stats")
+		.then((res) => res.json())
+		.then((data) => {
+			debugOutput.innerText = JSON.stringify(data, 0, 2);
+
+			console.debug(data);
+		});
+}
+
+init();
