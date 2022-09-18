8c16bc8d4d70d4a38dcf1d4a81cd9dc8ffdbd046
Zane Shaw <admin@triangle100.com>
2022-09-18 01:17:33 +0000

    Add `express` and `socket.io`

--- main.js
+++ main.js
@@ -1,13 +1,14 @@
-const http = require('http');
+const http = require("http");
+const express = require("express");
+const { Server } = require("socket.io");
 
-const hostname = '192.168.0.39';
+const hostname = "192.168.0.39";
 const port = 3000;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.write('hello world!');
-  res.end();
+  res.setHeader("Content-Type", "text/plain");
+  res.end("Hello world!");
 });
 
 server.listen(port, hostname, () => {
