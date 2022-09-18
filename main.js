6720d2bf95afab8e93de4f60b099fd5921669696
Zane Shaw <admin@triangle100.com>
2022-09-18 01:26:46 +0000

    Use express server

--- main.js
+++ main.js
@@ -1,16 +1,16 @@
 const http = require("http");
 const express = require("express");
-const { Server } = require("socket.io");
 
-const hostname = "192.168.0.39";
+const app = express();
+const server = http.createServer(app);
+
+const hostname = "0.0.0.0";
 const port = 3000;
-
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader("Content-Type", "text/plain");
-  res.end("Hello world!");
-});
 
 server.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
 });
+
+app.get("/", function (req, res) {
+  res.send('Hello world!');
+})
