d5ce93fff12a9ccd715e0265afdb4184ec612c1d
Zane Shaw <admin@triangle100.com>
2022-09-19 00:36:27 +0000

    Add response time debug

--- public/app.js
+++ public/app.js
@@ -1,12 +1,18 @@
 const debugOutput = document.getElementById("debug");
+const debugResponse = document.getElementById("response-time");
 
 function init() {
+	const start = window.performance.now();
 	fetch(window.location.href + "stats")
 		.then((res) => res.json())
 		.then((data) => {
-			debugOutput.innerText = JSON.stringify(data, 0, 2);
+			const end = window.performance.now();
+			const responseTime = end - start;
 
-			console.debug(data);
+			debugOutput.innerText = JSON.stringify(data, 0, 2);
+			debugResponse.innerText = `Response time: ${(responseTime / 1000).toFixed(2)}s`
+
+			console.debug(data, responseTime);
 		});
 }
 
