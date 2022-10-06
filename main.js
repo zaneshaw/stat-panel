4d4baa1241f242e8dca9c16db86df1120f048481
Zane Shaw <zaneshaw.home@gmail.com>
2022-10-06 11:25:53 +1100

    Fix fetch

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ import http from "http";
 import express from "express";
 import si from "systeminformation";
 import path from "path";
+import fetch from "node-fetch";
 
 const app = express();
 const server = http.createServer(app);
